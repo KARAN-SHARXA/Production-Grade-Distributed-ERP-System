e1d212d0eb199c2f358bccb20ecd6c3e5a7d7242
Karan Sharma <karansharma2302578@gmail.com>
2026-08-18 23:05:28 +0530

    attach the database with project

--- services/auth-service/app/main.py
+++ services/auth-service/app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.core.database import engine
+
 
 app = FastAPI(
     title="ERP Authentication Service",
     version="1.0.0"
 )
+
+
+@app.get("/")
+async def root():
+    return {
+        "message": "ERP Auth Service is running"
+    }
 
 
 @app.get("/health")
@@ -14,8 +24,20 @@ async def health_check():
     }
 
 
-@app.get("/")
-async def root():
-    return {
-        "message": "ERP Auth Service is running"
-    }
+@app.get("/db-test")
+async def database_test():
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT 1"))
+
+            return {
+                "status": "success",
+                "database": "connected",
+                "result": result.scalar()
+            }
+
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
